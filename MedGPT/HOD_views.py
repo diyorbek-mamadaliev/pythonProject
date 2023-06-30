37e72c45f43c309668b04df93339e66394ae7437
diyorbekmamadaliev <mamadaliev.dr19@gmail.com>
2023-06-30 18:56:09 +0500

    Second update

--- MedGPT/HOD_views.py
+++ MedGPT/HOD_views.py
@@ -1,1 +1,26 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,HttpResponse
+import requests
+from django.http import JsonResponse
+import openai
+from openai import api_key
+import requests
+from IPython.display import HTML
+
+API_KEY = "YEA0AN5T0V1YRL33L93469UXAE58X14AU1I"
+
+
+def HOME(request):
+    return render(request, 'Hod/home.html')
+
+
+def hod_chat(request):
+    if request.method == 'POST' and request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest':
+        input_text = request.POST.get('input_text', '')  # Get the input text from the AJAX request
+
+        url = f"https://api.betterapi.net/youchat?inputs={input_text}&key={API_KEY}"  # Set API URL
+        response = requests.get(url).json()  # Load JSON from API
+        generated_text = response.get('generated_text', '')  # Extract the generated text
+
+        return JsonResponse({'generated_text': generated_text})
+
+    return JsonResponse({'error': 'Invalid request'})
--- MedGPT/urls.py
+++ MedGPT/urls.py
@@ -2,9 +2,21 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from .import views,HOD_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('base/', views.BASE, name='base'),
+    # Login Path
+    path('', views.LOGIN, name='login'),
+    path('doLogin', views.doLogin, name='doLogin'),
+    path('doLogout', views.doLogout, name='logout'),
+    # Main
+    path('Hod/Home', HOD_views.HOME, name='home'),
+    path('hod-chat/', HOD_views.hod_chat, name='hod_chat'),
+    # Profile
+    path('profile', views.PROFILE, name='profile'),
+    path('Profile/update', views.PROFILE_UPDATE, name='profile_update'),
 
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
