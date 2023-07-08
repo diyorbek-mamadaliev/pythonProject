0b5a9d8e76350f0313711ef307510b51969b28d9
diyorbekmamadaliev <mamadaliev.dr19@gmail.com>
2023-07-08 23:11:21 +0500

    Beta Version update last

--- MedGPT/HOD_views.py
+++ MedGPT/HOD_views.py
@@ -4,6 +4,7 @@ from django.http import JsonResponse
 import openai
 from openai import api_key
 import requests
+import json
 from IPython.display import HTML
 from django.contrib.auth.decorators import login_required
 
@@ -26,17 +27,29 @@ def BOT(request):
     return render(request, 'Hod/bot.html')
 
 
+import requests
+
 def hod_chat(request):
     if request.method == 'POST' and request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest':
         input_text = request.POST.get('input_text', '')  # Get the input text from the AJAX request
 
-        url = f"https://api.betterapi.net/youchat?inputs={input_text}&key={API_KEY}"  # Set API URL
-        response = requests.get(url).json()  # Load JSON from API
-        generated_text = response.get('generated_text', '')  # Extract the generated text
+        url = "https://chatgpt-api7.p.rapidapi.com/ask"  # Set API URL
+        payload = {"query": input_text}
+        headers = {
+            "Content-Type": "application/json",
+            "X-RapidAPI-Key": "eba8330802mshbce627085fbb152p1609f5jsnc3bcb8954510",
+            "X-RapidAPI-Host": "chatgpt-api7.p.rapidapi.com"
+        }
 
-        return JsonResponse({'generated_text': generated_text})
+        response = requests.post(url, json=payload, headers=headers)
+        generated_text = response.json()
+
+        generated_text_str = json.dumps(generated_text)  # Convert generated_text to a string
+
+        return JsonResponse({'generated_text': generated_text_str})
 
     return JsonResponse({'error': 'Invalid request'})
+
 
 
 def chem_chat(request):
